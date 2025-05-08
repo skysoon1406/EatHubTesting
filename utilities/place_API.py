b10246a56dba863ec2744da623e6c8fab01e6a5d
Soon <skysoon1406@gmail.com>
2025-05-08 15:10:00 +0800

    rename place_API

--- utilities/place_API.py
+++ utilities/place_API.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 import os
 
 def text_search(keyword, location, radius):
-    API_KEY = os.getenv("Google_API_KEY")
+    API_KEY = os.getenv('Google_API_KEY')
     query = keyword
-    language = "zh-TW"
-    type = "food"
-    url = f"https://maps.googleapis.com/maps/api/place/textsearch/json?query={query}&location={location}&radius={radius}&language={language}&type={type}&key={API_KEY}"
+    language = 'zh-TW'
+    type = 'food'
+    url = f'https://maps.googleapis.com/maps/api/place/textsearch/json?query={query}&location={location}&radius={radius}&language={language}&type={type}&key={API_KEY}'
 
     all_data = []
 
@@ -16,11 +16,11 @@ def text_search(keyword, location, radius):
         response = requests.get(url)
         data = response.json()
 
-        if data.get("status") != "OK":
+        if data.get('status') != 'OK':
             break
 
         all_data.append(data)
-        next_page_token = data.get("next_page_token")
+        next_page_token = data.get('next_page_token')
 
         if next_page_token:
             time.sleep(3)
