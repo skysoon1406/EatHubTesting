843ab9498c2332e93be2c64e317ec2c580fc9181
Yichen Pan <s301751752@gmail.com>
2025-05-09 12:45:37 +0800

    feat(place_api): add count

--- utilities/place_api.py
+++ utilities/place_api.py
@@ -3,15 +3,14 @@ import time
 from dotenv import load_dotenv
 import os
 
-def text_search(keyword, location, radius):
+def text_search(keyword, location, radius, count=61):
     load_dotenv()
     API_KEY = os.getenv('GOOGLE_API_KEY')
     query = keyword
     language = 'zh-TW'
-    type = 'food'
-    url = f'https://maps.googleapis.com/maps/api/place/textsearch/json?query={query}&location={location}&radius={radius}&language={language}&type={type}&key={API_KEY}'
+    url = f'https://maps.googleapis.com/maps/api/place/textsearch/json?query={query}&location={location}&radius={radius}&language={language}&key={API_KEY}'
 
-    all_data = []
+    result = []
 
     while True:
         response = requests.get(url)
@@ -19,13 +18,27 @@ def text_search(keyword, location, radius):
 
         if data.get('status') != 'OK':
             break
+        
+        for place in data.get('results', []):
+            result.append({
+            'name': place.get('name'),
+            'address': place.get('formatted_address'),
+            'google_rating': place.get('rating'),
+            'latitude': place['geometry']['location']['lat'],
+            'longitude': place['geometry']['location']['lng'],
+            'types': place.get('types', []),
+            'place_id': place.get('place_id'),
+            })
 
-        all_data.append(data)
+            if len(result) >= count:
+                return result
+
         next_page_token = data.get('next_page_token')
 
         if next_page_token:
             time.sleep(3)
+            url = f'https://maps.googleapis.com/maps/api/place/textsearch/json?pagetoken={next_page_token}&key={API_KEY}'
         else:
             break
 
-    return all_data
+    return result
