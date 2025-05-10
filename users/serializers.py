2f23ce1eb0ea81c45ae3edac060c592cdcf17379
Ting <q.e.d14702@gmail.com>
2025-05-10 20:32:46 +0800

    feat: add user signup API

---
+++ users/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import User
+from django.contrib.auth.hashers import make_password
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source="user_name")
+    firstname = serializers.CharField(source="first_name")
+    lastname = serializers.CharField(source="last_name")
+
+    class Meta:
+        model = User
+        fields = ["firstname", "lastname", "username", "email", "password"]
+
+    def validate_password(self, value):
+        return make_password(value)
+
+    def create(self, validated_data):
+        return User.objects.create(**validated_data)
