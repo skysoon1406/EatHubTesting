87ff0cf795dd85cd563868983335fef3f2583169
Soon <skysoon1406@gmail.com>
2025-05-10 13:11:31 +0800

    feat: search restaurant still fail

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from restaurants.views import RestaurantSearchView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/restaurants/', include('restaurants.urls')),
+    path('api/v1/restaurants/search/', RestaurantSearchView.as_view(), name='restaurant-search'),
 ]
