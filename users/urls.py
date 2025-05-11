c3162947c2dc9c0ec35628436f2c5e1edbed0ea5
Soon <skysoon1406@gmail.com>
2025-05-11 12:40:47 +0800

    feat: add token_required decorators

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("v1/auth/signup", views.SignupView.as_view(), name="signup"),
     path("v1/auth/login", views.LoginView.as_view(), name="login"),
+    path("v1/auth/me", views.MeView.as_view(), name="me"),
 ]
