9d2524b6c5de296f61e365c777e285aaedf3ff49
Yichen Pan <s301751752@gmail.com>
2025-05-13 12:38:36 +0800

    fix: camelcase

--- restaurants/views.py
+++ restaurants/views.py
@@ -4,21 +4,21 @@ from django.shortcuts import render
 POST api/v1/restaurants/
 
 body = {
-    'flavors': ['日式', '韓式', '清蒸'],
-    'mains': ['雞肉', '豬肉', '牛肉'],
-    'staples': ['米飯', '麵條'],
-    'userLocation': {
-        'latitude': 25.0424,
-        'longitude': 121.5137
+    "flavors": ["日式", "韓式", "清蒸"],
+    "mains": ["雞肉", "豬肉", "牛肉"],
+    "staples": ["米飯", "麵條"],
+    "userLocation": {
+        "latitude": 25.0424,
+        "longitude": 121.5137
     }
 }
 
-api = {
-    'recommended':[
+result = {
+    "recommended":[
         {
             "name": "麵屋武藏 本店",
             "address": "100台灣台北市中正區忠孝西路一段36號B1",
-            "google_rating": 4.3,
+            "googleRating": 4.3,
             "latitude": 25.0459993,
             "longitude": 121.5170414,
             "types": [
@@ -27,7 +27,8 @@ api = {
                 "point_of_interest",
                 "establishment"
             ],
-            "place_id": "ChIJpabH4qapQjQRqmx_I-V8sUg"
+            "placeId": "ChIJpabH4qapQjQRqmx_I-V8sUg",
+            "imageUrl": "https:"
         },
         {
             "name": "鳥人拉麵-西門店",
