f058d8185a3df145b472c35070272f94de9409b3
Soon <skysoon1406@gmail.com>
2025-05-13 10:49:57 +0800

    fix: update login authentication logic

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("v1/auth/signup", views.SignupView.as_view(), name="signup"),
-    path("v1/auth/login", views.LoginView.as_view(), name="login"),
-    path("v1/auth/me", views.MeView.as_view(), name="me"),
-    path("v1/auth/logout", views.LogoutView.as_view(), name="logout"),
+    path("v1/auth/signup", views.SignupView.as_view()),
+    path("v1/auth/login", views.LoginView.as_view()),
+    path("v1/auth/me", views.MeView.as_view()),
+    path("v1/auth/logout", views.LogoutView.as_view()),
 ]
