33b0e8711c26f6b69f9fe6c2fd7e260bca0ba041
Ting <q.e.d14702@gmail.com>
2025-05-14 18:29:53 +0800

    feat: add create restaurant reviews

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.recommendRestaurants)
+    path('', views.recommendRestaurants),
+    path('<uuid:restaurant_uuid>/reviews', views.create_review),
 ]
