130b23439ead76cc85a134283698d9d645bd588c
Ting <q.e.d14702@gmail.com>
2025-05-14 12:15:55 +0800

    fix: config urls

--- promotions/views.py
+++ promotions/views.py
@@ -1,3 +1,71 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Coupon
+from .serializers import CouponSerializer
 
-# Create your views here.
+"""
+GET /api/v1/coupons/<uuid>/
+result = {
+    "restaurant": {
+        "name": "五倍咖啡",
+        "imgUrl": "https://example.com/restaurant.jpg"
+    },
+    "serialNumber": "test20240510",
+    "endedAt": "2025-05-10T05:08:40Z",
+    "createdAt": "2025-05-10T05:09:41.174907Z",
+    "title": "test01",
+    "description": "滿500折50",
+    "discountType": "amount",
+    "discountValue": 50,
+    "total": 100,
+    "isArchived": false,
+    "startedAt": "2025-05-10T05:09:39Z",
+    "uuid": "cd37115d-ce26-42e1-a71b-ceb66d54e163"
+}
+
+POST /api/v1/coupons/
+body = {
+  "serialNumber": "test20240510",
+  "title": "test01",
+  "description": "滿500折50",
+  "discountType": "amount",
+  "discountValue": 50,
+  "total": 100,
+  "startedAt": "2025-05-10T05:09:39Z",
+  "endedAt": "2025-05-10T05:08:40Z",
+  "isArchived": false
+}
+#  201 Created
+result = {
+  "serialNumber": "test20240510",
+  "title": "test01",
+  "description": "滿500折50",
+  "discountType": "amount",
+  "discountValue": 50,
+  "total": 100,
+  "isArchived": false,
+  "startedAt": "2025-05-10T05:09:39Z",
+  "endedAt": "2025-05-10T05:08:40Z",
+  "createdAt": "2025-05-13T07:15:41.120000Z",
+  "uuid": "cd37115d-ce26-42e1-a71b-ceb66d54e163",
+  "restaurant": {
+    "name": "五倍咖啡",
+    "imgUrl": "https://example.com/restaurant.jpg"
+  }
+}
+#  400 Bad Request 
+result = {
+  "serialNumber": ["此欄位為必填"],
+  "discountType": ["此欄位為必填"],
+  "discountValue": ["此欄位為必填"]
+}
+"""
+class CouponViewSet(viewsets.ModelViewSet):
+    queryset = Coupon.objects.filter(is_archived=False)
+    serializer_class = CouponSerializer
+    lookup_field = 'uuid'  # 使用 uuid 作為查詢欄位
+
+    #自動把目前登入的使用者的 restaurant 塞進去
+    def perform_create(self, serializer):
+        user = self.request.user
+        serializer.save(restaurant=user.restaurant)
--- utilities/place_api.py
+++ utilities/place_api.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 import os
 
 GOOGLE_MAP_API_BASE_URL = 'https://maps.googleapis.com/maps/api/place'
+load_dotenv()
 
 def text_search(keyword, location, radius, count=61):
-    load_dotenv()
     API_KEY = os.getenv('GOOGLE_API_KEY')
     query = keyword
     language = 'zh-TW'
@@ -23,13 +23,17 @@ def text_search(keyword, location, radius, count=61):
         
         for place in data.get('results', []):
             result.append({
-            'name': place.get('name'),
-            'address': place.get('formatted_address'),
-            'rating': place.get('rating'),
-            'latitude': place['geometry']['location']['lat'],
-            'longitude': place['geometry']['location']['lng'],
-            'types': place.get('types', []),
-            'placeId': place.get('place_id'),
+                'name': place.get('name'),
+                'address': place.get('formatted_address'),
+                'rating': place.get('rating'),
+                'latitude': place['geometry']['location']['lat'],
+                'longitude': place['geometry']['location']['lng'],
+                'types': place.get('types', []),
+                'placeId': place.get('place_id'),
+                'googlePhotoReference': (
+                    place.get('photos', [{}])[0].get('photo_reference')
+                    if place.get('photos') else None
+                )
             })
 
             if len(result) >= count:
@@ -44,3 +48,16 @@ def text_search(keyword, location, radius, count=61):
             break
 
     return result
+
+def get_google_photo(photo_reference, max_width = 400):
+    API_KEY = os.getenv('GOOGLE_API_KEY')
+
+    url = (
+        'https://maps.googleapis.com/maps/api/place/photo'
+        f'?maxwidth={max_width}&photo_reference={photo_reference}&key={API_KEY}'
+    )
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.content
+    return None
