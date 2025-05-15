6c4020081d688553a8d838dd67ce4b7adf59d044
Soon <skysoon1406@gmail.com>
2025-05-15 15:47:05 +0800

    feat(restaurant/views): add test for Cloudinary upload image function URLS

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.recommendRestaurants)
+    path('', views.recommendRestaurants),
+    path('test_upload_image', views.test_upload_image),
 ]
