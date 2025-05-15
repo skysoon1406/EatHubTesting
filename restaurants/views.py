967e4a36f0d3d5e1890138cde7f3988c4d9fa2da
Soon <skysoon1406@gmail.com>
2025-05-15 10:44:22 +0800

    Refactor token_required decorator to support both FBV and CBV

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,11 +1,12 @@
 from rest_framework.decorators import api_view, permission_classes
-from users.utils import token_required
+from users.utils import token_required_fbv
 from .models import Review, Restaurant
 from .serializers import ReviewSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from utilities.place_api import text_search
 from utilities.openai_api import openai_api
+from users.models import User
 
 """
 POST api/v1/restaurants/
@@ -80,19 +81,19 @@ def recommendRestaurants(request):
     return Response({'result': result}, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
-@token_required
+@token_required_fbv
 def create_review(request, restaurant_uuid):
     try:
-        retsaurant=ReviewSerializer.objects.get(uuid=restaurant_uuid)
+        restaurant=Restaurant.objects.get(uuid=restaurant_uuid)
     except Restaurant.DoesNotExist:
         return Response({'error':'找不到該餐廳'}, status=status.HTTP_404_NOT_FOUND)
     
-    data=request.data.copy()
-    data['restaurant']=retsaurant.id
-    data['user']=request.user.id
+    data = request.data.copy()
+    user = User.objects.get(uuid=request.user_uuid)
 
-    serializer= ReviewSerializer(data=data)
+    serializer = ReviewSerializer(data=data)
+    print(serializer.fields)
     if serializer.is_valid():
-        serializer.save()
+        serializer.save(user=user, restaurant=restaurant)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
