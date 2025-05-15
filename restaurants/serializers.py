26781e2021b7390ce714759ef99c6e19ee6625fc
Soon <skysoon1406@gmail.com>
2025-05-15 14:38:37 +0800

    wip

--- restaurants/serializers.py
+++ restaurants/serializers.py
@@ -4,5 +4,5 @@ from rest_framework import serializers
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model= Review
-        fields=['uuid', 'user', 'restaurant', 'rating', 'content', 'img_url', 'created_at']
+        fields=['uuid', 'user', 'restaurant', 'rating', 'content', 'image_url', 'created_at']
         read_only_fields=['uuid', 'create_at', 'user','restaurant']
