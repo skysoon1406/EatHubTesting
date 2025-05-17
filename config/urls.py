80971edf50b9fc09a4a1536e4d617b7872f672d5
Soon <skysoon1406@gmail.com>
2025-05-17 12:55:25 +0800

    refactor(api): rename userCoupons to user-coupons for RESTful naming consistency

--- config/urls.py
+++ config/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('api/v1/auth/', include('users.urls')),
     path('api/v1/coupons/', include('promotions.coupon_urls')),
     path('api/v1/restaurants/', include('restaurants.urls')),
-    path('api/v1/userCoupons/', include('users.user_coupon_urls')),
+    path('api/v1/user-coupons/', include('users.user_coupon_urls')),
 ]
