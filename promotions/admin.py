0c076c05f0c235c4b6869e0fa62324ef9349a815
panda <qWwe79790922@gmail.com>
2025-05-26 14:04:30 +0800

    feat(admin): add all admin classes and refine existing admin settings

--- promotions/admin.py
+++ promotions/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import Coupon
+from .models import Coupon, Promotion
 
 @admin.register(Coupon)
 class CouponAdmin(admin.ModelAdmin):
-    list_display = ['title', 'serial_number', 'restaurant', 'started_at', 'ended_at', 'is_archived']
-    
+    list_display = ('title', 'serial_number', 'restaurant', 'started_at', 'ended_at', 'is_archived')
+    autocomplete_fields = ['restaurant']
+    search_fields = ('title', 'restaurant__name')
+
+@admin.register(Promotion)
+class PromotionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'restaurant', 'started_at', 'ended_at', 'is_archived')
+    search_fields = ('title', 'restaurant__name')
+    list_filter = ('is_archived',)
+    ordering = ('-started_at',)  
+    autocomplete_fields = ['restaurant'] 
