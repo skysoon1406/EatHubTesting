de75a99fed7e86c39eb5fce8c496aeab81ee9170
Soon <skysoon1406@gmail.com>
2025-05-26 14:22:00 +0800

    feat: add merchant promotion detail API

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/auth/', include('users.urls')),
     path('api/v1/coupons/', include('promotions.coupon_urls')),
+    path('api/v1/promotions/', include('promotions.urls')),
     path('api/v1/restaurants/', include('restaurants.urls')),
     path('api/v1/favorites/', include('users.favorite_urls')),
     path('api/v1/user-coupons/', include('users.user_coupon_urls')),
