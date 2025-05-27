cedd763b31920057aa1e1d36695b21c8479f3403
enen <trigunbob123@gmail.com>
2025-05-27 19:00:08 +0800

    feat: implement backend API for merchant dashboard news detail

--- config/urls.py
+++ config/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('api/v1/restaurants/', include('restaurants.urls')),
     path('api/v1/favorites/', include('users.favorite_urls')),
     path('api/v1/user-coupons/', include('users.user_coupon_urls')),
+
+    path('api/v1/promotions/', include('promotions.promotion_urls')),
 ]
---
+++ promotions/promotion_urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import PromotionDetailView
+
+urlpatterns = [
+    path('<uuid:uuid>', PromotionDetailView.as_view()),
+]
