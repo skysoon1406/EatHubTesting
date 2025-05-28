4202c4552ab15a5313ba91b086bd7e6225eb6db5
Soon <skysoon1406@gmail.com>
2025-05-28 15:16:07 +0800

    feat: add PATCH API for achiving coupons and promotions

--- config/urls.py
+++ config/urls.py
@@ -5,8 +5,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/auth/', include('users.urls')),
     path('api/v1/coupons/', include('promotions.coupon_urls')),
+    path('api/v1/promotions/', include('promotions.promotion_urls')),
+
     path('api/v1/restaurants/', include('restaurants.urls')),
     path('api/v1/favorites/', include('users.favorite_urls')),
     path('api/v1/user-coupons/', include('users.user_coupon_urls')),
-    path('api/v1/merchants/', include('promotions.promotion_urls')),
+    path('api/v1/merchants/', include('promotions.merchant_urls')),
 ]
