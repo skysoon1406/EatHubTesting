e8baa0321bd3edf423cc8773a6870233c34227df
enen <trigunbob123@gmail.com>
2025-06-04 12:05:32 +0800

    refactor: rename admin route path

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('5th-floor/', admin.site.urls),
     path('api/v1/auth/', include('users.urls')),
     path('api/v1/coupons/', include('promotions.coupon_urls')),
     path('api/v1/promotions/', include('promotions.promotion_urls')),
