10fb19411a0f545fc080967e0495c877937ca916
Soon <skysoon1406@gmail.com>
2025-06-05 16:58:44 +0800

    feat : add merchant VIP expiry date to PaymentOrder Serializer

--- payments/serializers.py
+++ payments/serializers.py
@@ -7,6 +7,7 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = ['uuid', 'name', 'plan_type', 'amount', 'interval_days']
 
 class PaymentOrderSerializer(serializers.ModelSerializer):
+    vip_expiry = serializers.DateField(source='subscription.ended_at', read_only=True)
     class Meta:
         model = PaymentOrder
-        fields = ['order_id', 'amount', 'is_paid', 'transaction_id', 'created_at']
+        fields = ['order_id', 'amount', 'is_paid', 'transaction_id', 'created_at','vip_expiry']
