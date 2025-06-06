2ed76506fca4c5af6031abe0f615a7c54541de37
Soon Lau <skysoon1406@gmail.com>
2025-06-06 17:22:23 +0800

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -120,10 +120,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+    )
     }
-}
 
 
 # Password validation
